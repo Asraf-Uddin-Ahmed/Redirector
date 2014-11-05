664009adbf853d088a40c3a2a8ed8c2d12f7f14c
ratul <13ratul@gmail.com>
2014-11-05 09:28:02 +0600

    extract chrome_storage portion to different file.

---
+++ chrome_storage.js
@@ -0,0 +1,40 @@
+
+REDIRECTOR.chromeStorage = function () {
+    var objToReturn = {};
+
+    /*
+    PRIVATE
+    */
+    var objRedirect = {};
+
+    var reloadRedirectObject = function () {
+        chrome.storage.sync.get(function (items) {
+            var obj = items['RedirectList'];
+            objRedirect = jQuery.extend({}, obj);
+        });
+    }
+
+    var saveRedirectObject = function (obj) {
+        chrome.storage.sync.set({ 'RedirectList': obj }, function () {
+            reloadRedirectObject();
+        });
+    }
+
+    var pushUrlByWord = function (word, url) {
+        objRedirect[word] = url;
+        return objRedirect;
+    }
+
+    var getRedirectObject = function () {
+        return objRedirect;
+    }
+
+    /*PUBLIC*/
+    objToReturn.reloadRedirectObject = reloadRedirectObject;
+    objToReturn.saveRedirectObject = saveRedirectObject;
+    objToReturn.pushUrlByWord = pushUrlByWord;
+    objToReturn.getRedirectObject = getRedirectObject;
+
+    return objToReturn;
+}();
+
--- options.js
+++ options.js
@@ -1,43 +1,4 @@
 
-REDIRECTOR.chromeStorage = function () {
-    var objToReturn = {};
-
-    /*
-    PRIVATE
-    */
-    var objRedirect = {};
-    
-    var reloadRedirectObject = function () {
-        chrome.storage.sync.get(function (items) {
-            var obj = items['RedirectList'];
-            objRedirect = jQuery.extend({}, obj);
-        });
-    }
-
-    var saveRedirectObject = function (obj) {
-        chrome.storage.sync.set({ 'RedirectList': obj }, function () {
-            reloadRedirectObject();
-        });
-    }
-
-    var pushUrlByWord = function (word, url) {
-        objRedirect[word] = url;
-        return objRedirect;
-    }
-
-    var getRedirectObject = function () {
-        return objRedirect;
-    }
-
-    /*PUBLIC*/
-    objToReturn.reloadRedirectObject = reloadRedirectObject;
-    objToReturn.saveRedirectObject = saveRedirectObject;
-    objToReturn.pushUrlByWord = pushUrlByWord;
-    objToReturn.getRedirectObject = getRedirectObject;
-
-    return objToReturn;
-}();
-
 
 $(function () {
     
@@ -58,7 +19,7 @@ $(function () {
         var blockWord = $("#blockWord").val();
         var redirectUrl = $("#redirectUrl").val();
         $("#blockWord").val("");
-        $("#redirectUrl").val("");
+        $("#redirectUrl").val("https://");
 
         var objRedirect = REDIRECTOR.chromeStorage.pushUrlByWord(blockWord, redirectUrl);
         REDIRECTOR.chromeStorage.saveRedirectObject(objRedirect);
